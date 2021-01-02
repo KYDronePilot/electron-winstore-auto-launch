439cb97b4ffc9646645c4bb0cdbbf570156b3eba
KYDronePilot <kydronepilotdev@gmail.com>
2021-01-02 11:48:58 -0500

    Heavily modified library
    - Just need to be able to get the task.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-let appModel: any = null
-
 export enum StartupTaskState {
     disabled,
     disabledByUser,
@@ -19,138 +17,33 @@ export class WindowsStoreAutoLaunch {
      * Returns the windows.applicationModel namespace for this model. If the app is not
      * running inside an appx container, this will fail.
      *
-     * @static
-     * @param {(reason: string) => void} [reject]
-     * @returns
-     * @memberof WindowsStoreAutoLaunch
+     * @returns windows.applicationModel namespace
      */
-    private static getAppModel(reject?: (reason: string) => void) {
-        try {
-            if (process.platform !== 'win32') {
-                if (reject) {
-                    return reject(`Platform is not win32`)
-                } else {
-                    throw new Error('Platform is not win32')
-                }
-            }
-
-            return appModel || require('@nodert-win10-rs3/windows.applicationmodel')
-        } catch (error) {
-            if (reject) {
-                return reject(error)
-            } else {
-                throw error
-            }
+    private static getAppModel(): any {
+        if (process.platform !== 'win32') {
+            throw new Error('Platform is not win32')
         }
+        return require('@nodert-win10-rs3/windows.applicationmodel')
     }
 
     /**
-     * Returns all startupTasks for this package.
+     * Returns the specified startupTask.
      *
-     * @static
-     * @returns {Promise<Array<StartupTask>>}
-     * @memberof WindowsStoreAutoLaunch
+     * @param taskId - ID of the task to fetch
+     * @returns The task if one exists
      */
-    static getStartupTasks(): Promise<Array<StartupTask>> {
+    static async getStartupTask(taskId: string): Promise<StartupTask | undefined> {
         return new Promise((resolve, reject) => {
-            const appModel = WindowsStoreAutoLaunch.getAppModel(reject)
-
-            appModel.StartupTask.getForCurrentPackageAsync(
-                (error: Error, tasks: Array<StartupTask>) => {
-                    if (error) {
-                        return reject(error)
-                    }
-
-                    resolve(tasks)
+            const appModel = WindowsStoreAutoLaunch.getAppModel()
+            appModel.StartupTask.getAsync(taskId, (error: Error, task: StartupTask) => {
+                if (error) {
+                    return reject(error)
                 }
-            )
-        })
-    }
-
-    /**
-     * Returns the first found startupTask.
-     *
-     * @static
-     * @returns {(Promise<StartupTask | null>)}
-     * @memberof WindowsStoreAutoLaunch
-     */
-    static async getStartupTask(): Promise<StartupTask | null> {
-        const startupTasks = await WindowsStoreAutoLaunch.getStartupTasks()
-
-        if (startupTasks && startupTasks.length > 0) {
-            const [startupTask] = startupTasks
-            return startupTask
-        } else {
-            return null
-        }
-    }
-
-    /**
-     * Returns a StartupTaskState if a StartupTask is found - or null if
-     * we could not find one.
-     *
-     * @static
-     * @returns {(Promise<StartupTaskState | null>)}
-     * @memberof WindowsStoreAutoLaunch
-     */
-    static async getStatus(): Promise<StartupTaskState | null> {
-        // We're assuming that this app has just one startup task, so let's go for it
-        const startupTask = await WindowsStoreAutoLaunch.getStartupTask()
-
-        if (startupTask) {
-            return startupTask.state
-        } else {
-            return null
-        }
-    }
-
-    /**
-     * Takes the first startup task and attempts to disable it.
-     *
-     * @static
-     * @returns {(Promise<boolean | null>)}
-     * @memberof WindowsStoreAutoLaunch
-     */
-    static async disable(): Promise<boolean | null> {
-        // We're assuming that this app has just one startup task, so let's go for it
-        const startupTask = await WindowsStoreAutoLaunch.getStartupTask()
-
-        if (startupTask) {
-            startupTask.disable()
-            return true
-        } else {
-            return null
-        }
-    }
-
-    /**
-     * Takes the first startup task and attempts to enable it.
-     *
-     * @static
-     * @returns {(Promise<StartupTaskState | null>)}
-     * @memberof WindowsStoreAutoLaunch
-     */
-    static async enable(): Promise<StartupTaskState | null> {
-        // We're assuming that this app has just one startup task, so let's go for it
-
-        function enableTask(startupTask: StartupTask): Promise<StartupTaskState> {
-            return new Promise((resolve, reject) => {
-                startupTask.requestEnableAsync((error: Error, newStatus: StartupTaskState) => {
-                    if (error) {
-                        return reject(error)
-                    }
-
-                    resolve(newStatus)
-                })
+                if (task) {
+                    return resolve(task)
+                }
+                return resolve(undefined)
             })
-        }
-
-        const startupTask = await WindowsStoreAutoLaunch.getStartupTask()
-
-        if (startupTask) {
-            return enableTask(startupTask)
-        } else {
-            return null
-        }
+        })
     }
 }
